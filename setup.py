abc99a67c9aeba60dff05a5dd1bc2f010d6c870e
Ahmed W. Aldesouky <awaleed12389@gmail.com>
2025-03-20 01:03:30 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Medical Bot',
+    version='0.0.0',
+    author='Ahmed Waleed Aldesouky',
+    author_email='awaleed12389@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
