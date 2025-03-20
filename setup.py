9f89c86381dc54584b4a120c70f66e7bb36d2d71
Ahmed W. Aldesouky <awaleed12389@gmail.com>
2025-03-20 06:56:38 +0200

    Trials Done!

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='Generative AI Medical Bot',
-    version='0.0.0',
-    author='Ahmed Waleed Aldesouky',
-    author_email='awaleed12389@gmail.com',
+    name = 'Salam-Hack Chatbot',
+    version= '0.0.0',
+    author= 'Ahmed Waleed Aldesouky',
+    author_email= 'awaleed12389@gmail.com',
     packages= find_packages(),
-    install_requires=[]
+    install_requires = []
+
 )
